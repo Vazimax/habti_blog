61ad93ce9f552d6f689bc94ebb1f14f6a6e77036
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-10-18 13:07:13 +0100

    updated about me page and the home one

--- app/models.py
+++ app/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
+
+
 class Post(models.Model):
+
+    categories = [
+        ('technology' , 'technology'),
+        ('science' , 'science'),
+        ('education' , 'education'),
+        ('business' , 'business'),
+        ('entrepreneurship' , 'entrepreneurship'),
+    ]
+
     title = models.CharField(max_length=100)
+    brief = models.TextField(max_length=500)
     description = models.TextField(max_length=10000)
+    image = models.ImageField(upload_to='blog_pics')
+    category = models.CharField(max_length=100,null=True,blank=True,choices=categories,)
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-date_posted',]
--- app/views.py
+++ app/views.py
@@ -3,8 +3,15 @@ from .models import Post
 
 
 def home(request):
+    posts = Post.objects.all()
 
-    return render(request,'home.html')
+    newest_posts = Post.objects.all().order_by('-date_posted')[0:1]
+
+    context = {
+        'posts' : posts,
+        'newest_posts' : newest_posts
+    }
+    return render(request,'home.html',context)
 
 def post(request,id):
     post = Post.objects.get(id=id)
